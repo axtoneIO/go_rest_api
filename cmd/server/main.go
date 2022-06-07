f891d9d0ece29e5f40bd8f9868cc26da0aaf8ece
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2022-06-07 17:31:00 -0500

    Fixed issue with .env file.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,19 +7,16 @@ import (
 
 	"github.com/axtoneIO/go_rest_api/internal/database"
 	transport_http "github.com/axtoneIO/go_rest_api/internal/transport/http"
-	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 // App - the struct which contains things like pointers
 // to database connections
-type App struct {}
+type App struct{}
 
 // Run - sets up our application
 func (app *App) Run() error {
 	fmt.Println("Setting up our App")
-
-	godotenv.Load(".env")
-
 	var err error
 
 	_, err = database.NewDatabase()
@@ -30,18 +27,18 @@ func (app *App) Run() error {
 	handler := transport_http.NewHandler()
 	handler.SetupRoutes()
 
-	if err := http.ListenAndServe("localhost:8000",handler.Router); err != nil {
+	if err := http.ListenAndServe("localhost:8000", handler.Router); err != nil {
 		log.Print(err)
 		return err
 	}
-	
+
 	return nil
 }
 
 func main() {
 	fmt.Println("Go REST API")
 	app := App{}
-	if err := app.Run(); err != nil{
+	if err := app.Run(); err != nil {
 		fmt.Println("Error starting up our REST API")
 		fmt.Println(err)
 	}
